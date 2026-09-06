7e6cc8308eb53e76003f2d4353a8f4779b446390
Donaldhemen <healthywayglobal25@gmail.com>
2026-09-06 09:43:50 +0100

    first commit

---
+++ Account1/src/Account.java
@@ -0,0 +1,26 @@
+public class Account {
+
+    private double balance;
+
+    public Account() {
+
+        balance = 0;
+    }
+    public double getBalance() {
+
+        return balance;
+    }
+
+    public void deposit(double amount) {
+
+        if(amount > 0) {
+            balance += amount;
+        }
+    }
+
+    public void withdraw(double amount) {
+       if(amount > 0 && amount <= balance) {
+           balance -= amount;
+       }
+    }
+}
